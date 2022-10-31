d316f52e1c10148fb7085e6ad36b76cb1a7c26d9
Mackenzie Leng <kenz.joy@outlook.com>
2022-10-31 10:03:07 -0600

    add association to customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 class Customer < ApplicationRecord
   validates_presence_of :name 
-  
+
   has_many :customer_items
+  has_many :items, through: :customer_items
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -7,5 +7,6 @@ RSpec.describe Customer, type: :model do
 
   describe 'relationships' do
     it { should have_many :customer_items }
+    it { should have_many(:items).through(:customer_items)}
   end
 end
